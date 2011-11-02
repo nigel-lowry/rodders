4dedb89e675a182a82dd70762abaf7db9ddad767
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2011-11-02 11:06:40 +0000

    Got some properly failing tests. Had to use USD rather than GBP for now because was getting encoding problem when running RSpec

--- spec/FixedOdds_spec.rb
+++ spec/FixedOdds_spec.rb
@@ -1,7 +1,46 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Rodders" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
+describe "FixedOdds" do
+
+	before(:each) do
+  	@fourToOne = FixedOdds.new "4/1"
+	end
+
+	describe "#new" do
+		it "should take one argument and return a FixedOdds object"
+	end
+
+	describe "#stake" do
+		it "should return nil for an uninitialised stake" do
+			@fourToOne.stake.should be_nil
+		end
+
+		it "should return the stake set" do
+			stakeAmount = 100
+			@fourToOne.stake = stakeAmount
+			@fourToOne.stake.should == stakeAmount
+		end
+	end
+
+	describe "#inReturn" do
+		it "should return nil if stake is uninitialised" do
+			@fourToOne.inReturn.should be_nil
+		end
+
+		it "should show that the full amount back on a winning 4/1 bet with a $100 stake is $500" do
+			@fourToOne.stake = 100
+			@fourToOne.inReturn.should == 500
+		end
+	end
+
+	describe "#profit" do
+		it "should return nil for an uninitialised stake" do
+			@fourToOne.profit.should be_nil
+		end
+
+		it "should return a profit of $400 on a $100 stake on a 4/1 bet" do 
+			@fourToOne.stake = 100
+			@fourToOne.profit.should == 400
+		end
+	end
 end
