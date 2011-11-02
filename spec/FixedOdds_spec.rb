33900c622469627f9bfccf28fdce36f6d5408cb9
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2011-11-02 13:57:53 +0000

    Added some more test cases which are already passing

--- spec/FixedOdds_spec.rb
+++ spec/FixedOdds_spec.rb
@@ -4,6 +4,7 @@ describe "FixedOdds" do
 
 	before(:each) do
   	@fourToOne = FixedOdds.new "4/1"
+  	@oneToFour = FixedOdds.new "1/4"
 	end
 
 	describe "#new" do
@@ -29,6 +30,11 @@ describe "FixedOdds" do
 			@fourToOne.stake = '$100'
 			@fourToOne.inReturn.should == '$500'
 		end
+
+		it "should show that the full amount back on a winning 1/4 bet with a $100 stake is $125" do
+			@oneToFour.stake = '$100'
+			@oneToFour.inReturn.should == '$125'
+		end
 	end
 
 	describe "#profit" do
@@ -37,6 +43,11 @@ describe "FixedOdds" do
 		it "should return a profit of $400 on a $100 stake on a 4/1 bet" do 
 			@fourToOne.stake = '$100'
 			@fourToOne.profit.should == '$400'
+		end
+
+		it "should return a profit of $25 on a $110 stake with a 1/4 bet" do
+			@oneToFour.stake = '$100'
+			@oneToFour.profit.should == '$25'
 		end
 	end
 end
