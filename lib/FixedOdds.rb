c33538949a3ca23d8bf87e58c64912fdbc354417
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2011-11-02 12:21:02 +0000

    Got #to_money conversion for stake on write

--- lib/FixedOdds.rb
+++ lib/FixedOdds.rb
@@ -1,21 +1,28 @@
 require 'money'
 
 class FixedOdds
-	attr_accessor :stake
 
 	def initialize fractionalOdds
 		@fractionalOdds = fractionalOdds
+	end
+
+	def stake=(value)
+		@stake = value.to_money
+	end
+
+	def stake
+		@stake
 	end
 
   def profit
   	/(?<numerator>\d+)\/(?<denominator>\d+)/ =~ @fractionalOdds
 
   	multiplier = numerator.to_f / denominator.to_f
-  	stake.to_money * multiplier
+  	stake * multiplier
   end
 
   def inReturn
-  	profit + stake.to_money
+  	profit + stake
   end
   
 end
