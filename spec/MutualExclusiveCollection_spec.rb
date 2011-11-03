3b5847e4ed2f80dab53b18bd09b0f97e583fa12d
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2011-11-03 00:34:36 +0000

    Now variable names reflect what they signify rather than relying on knowledge of relative merits of UK football teams

--- spec/MutualExclusiveCollection_spec.rb
+++ spec/MutualExclusiveCollection_spec.rb
@@ -3,27 +3,27 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "MutuallyExclusiveCollection" do
   
   before(:each) do    
-    @chelsea = FixedOdds.from_s '-275'
+    @good_team = FixedOdds.from_s '-275'
     @draw = FixedOdds.from_s '+429'
-    @accrington_stanley = FixedOdds.from_s '+915'
+    @bad_team = FixedOdds.from_s '+915'
 
-    @events = MutuallyExclusiveCollection.new [@draw, @accrington_stanley, @chelsea]
+    @events = MutuallyExclusiveCollection.new [@draw, @bad_team, @good_team]
   end
 
   it "should return Accrington Stanley as the underdog" do
-  	@events.underdog.should == @accrington_stanley
+  	@events.underdog.should == @bad_team
   end
 
   it "should return Chelsea as the favorite" do
-  	@events.favorite.should == @chelsea
+  	@events.favorite.should == @good_team
   end
 
   it "should return the events in descending probability" do
-  	@events.in_descending_probability.should == [@chelsea, @draw, @accrington_stanley]
+  	@events.in_descending_probability.should == [@good_team, @draw, @bad_team]
   end
 
   it "should return the events in ascending probability" do
-  	@events.in_ascending_probability.should == [@accrington_stanley, @draw, @chelsea]
+  	@events.in_ascending_probability.should == [@bad_team, @draw, @good_team]
   end
 
 end
