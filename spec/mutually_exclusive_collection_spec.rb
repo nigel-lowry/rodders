de9e5faf68e87999a6437e8b87b5bfd3930f4d11
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2012-04-14 22:09:50 +0100

    Move to existing class

--- spec/mutually_exclusive_collection_spec.rb
+++ spec/mutually_exclusive_collection_spec.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "MutuallyExclusiveCollection" do
@@ -8,6 +9,16 @@ describe "MutuallyExclusiveCollection" do
     @bad_team = FixedOdds.from_s '+915'
 
     @events = MutuallyExclusiveCollection.new [@draw, @bad_team, @good_team]
+
+    @bookmaker1outcome1 = FixedOdds.from_s '1.25'
+    @bookmaker1outcome2 = FixedOdds.from_s '3.9'
+    @bookmaker2outcome1 = FixedOdds.from_s '1.43'
+    @bookmaker2outcome2 = FixedOdds.from_s '2.85'
+
+    @bookmaker1 = MutuallyExclusiveCollection.new [@bookmaker1outcome1, @bookmaker1outcome2]
+    @bookmaker2 = MutuallyExclusiveCollection.new [@bookmaker2outcome1, @bookmaker2outcome2]
+
+    @bookmaker_vulnerable_to_arbitrage = MutuallyExclusiveCollection.new [@bookmaker2outcome1, @bookmaker1outcome2]
   end
 
   describe "#least_likely" do
@@ -31,6 +42,46 @@ describe "MutuallyExclusiveCollection" do
   describe "#in_ascending_probability" do
     it "is in ascending order of probability" do
       @events.in_ascending_probability.should == [@bad_team, @draw, @good_team]
+    end
+  end
+
+  describe "#sum_inverse_outcome" do
+    it "is 1.056 for bookmaker 1" do
+      @bookmaker1.sum_inverse_outcome.should be_within(0.001).of(1.056)
+    end
+
+    it "is 1.051 for bookmaker 2" do
+      @bookmaker2.sum_inverse_outcome.should be_within(0.001).of(1.051)
+    end
+  end
+
+  describe "#rational_bookmaker?" do
+    it "is true for bookmaker 1" do
+      @bookmaker1.rational_bookmaker?.should be
+    end
+
+    it "is true for bookmaker 2" do
+      @bookmaker2.rational_bookmaker?.should be
+    end
+
+    it "is false for vulnerable bookmaker" do
+      @bookmaker_vulnerable_to_arbitrage.rational_bookmaker?.should be_false
+    end
+  end
+
+  describe "#bookmakers_return_rate" do
+    it "is 5.34% for bookmaker 1" do
+      @bookmaker1.bookmakers_return_rate.should be_within(0.0001).of(0.0534)
+    end
+
+    it "is 4.78% for bookmaker 2" do
+      @bookmaker2.bookmakers_return_rate.should be_within(0.0001).of(0.0478)
+    end
+  end
+
+  describe "#other_amount" do
+    it "is £36.67 on outcome 2 on a £100.00 stake on outcome 1" do
+      @bookmaker_vulnerable_to_arbitrage.other_amount(stake: Money.from_fixnum(100, :GBP), odds: @bookmaker2outcome1, odds_other: @bookmaker1outcome2).should == Money.from_fixnum(36.67, :GBP)
     end
   end
 end
