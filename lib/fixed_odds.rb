9b483d684b5dd776347369bd1161f21520b98a7d
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2012-05-12 16:14:06 +0100

    Remove parentheses

--- lib/fixed_odds.rb
+++ lib/fixed_odds.rb
@@ -15,7 +15,7 @@ class FixedOdds
   # equivalent to '5/1'
   # @param [String] odds the odds in fractional, moneyline or decimal form
   # @return [FixedOdds]
-  def FixedOdds.from_s(odds)
+  def FixedOdds.from_s odds
     case
     when fractional_odds?(odds) then fractional_odds odds
     when moneyline_odds?(odds)  then moneyline_odds odds
@@ -27,21 +27,21 @@ class FixedOdds
   # tells if the odds are in fractional form
   # @param [String] odds the odds representation
   # @return [Boolean] to indicate if it matches
-  def FixedOdds.fractional_odds?(odds)
+  def FixedOdds.fractional_odds? odds
     odds =~ /^([1-9]\d*(\/|-to-)[1-9]\d*( (against|on))?|evens|even money)$/
   end
 
   # tells if the odds are in moneyline form
   # @param (see FixedOdds.fractional_odds?)
   # @return (see FixedOdds.fractional_odds?)
-  def FixedOdds.moneyline_odds?(odds)
+  def FixedOdds.moneyline_odds? odds
     odds =~ /^[+-][1-9]\d*$/ 
   end
 
   # tells if the odds are in decimal form
   # @param (see FixedOdds.fractional_odds?)
   # @return (see FixedOdds.fractional_odds?)
-  def FixedOdds.decimal_odds?(odds)
+  def FixedOdds.decimal_odds? odds
     odds =~ /^([1-9]\d*(\.\d+)?|\.\d+)$/ 
   end
 
@@ -56,7 +56,7 @@ class FixedOdds
   # * even money
   # @param [String] fractional odds in fractional form
   # @return (see FixedOdds.from_s)
-  def FixedOdds.fractional_odds(fractional)
+  def FixedOdds.fractional_odds fractional
     raise %{could not parse "#{fractional}" as fractional odds} unless self.fractional_odds?(fractional)
     return new(1.to_r) if fractional == 'evens' or fractional == 'even money' 
 
@@ -68,7 +68,7 @@ class FixedOdds
 
   # creates a new FixedOdds from a Rational
   # @param [Rational] fractional_odds the odds
-  def initialize(fractional_odds)
+  def initialize fractional_odds
     @fractional_odds = fractional_odds
   end
 
@@ -78,7 +78,7 @@ class FixedOdds
   # @note (see #from_s)
   # @param [String] moneyline odds in moneyline form
   # @return (see FixedOdds.from_s)
-  def FixedOdds.moneyline_odds(moneyline)
+  def FixedOdds.moneyline_odds moneyline
     raise %{could not parse "#{moneyline}" as moneyline odds} unless self.moneyline_odds?(moneyline)
     sign = moneyline[0]
     if sign == '+' then new(Rational(moneyline, 100))
@@ -91,7 +91,7 @@ class FixedOdds
   # * 2
   # @param [String] decimal odds in decimal form
   # @return (see FixedOdds.from_s)
-  def FixedOdds.decimal_odds(decimal)
+  def FixedOdds.decimal_odds decimal
     raise %{could not parse "#{decimal}" as decimal odds} unless self.decimal_odds?(decimal)
     new(decimal.to_r - 1)
   end
@@ -99,7 +99,7 @@ class FixedOdds
   # calculates the profit on a winning stake
   # @param [Money] stake the stake
   # @return [Money] the profit
-  def profit_on_stake(stake)
+  def profit_on_stake stake
     stake * @fractional_odds
   end
 
@@ -107,7 +107,7 @@ class FixedOdds
   # (the profit plus the stake)
   # @param (see #profit_on_stake)
   # @return [Money] the total returned
-  def total_return_on_stake(stake)
+  def total_return_on_stake stake
     profit_on_stake(stake) + stake
   end
 
