109c1098edebc0e814e89f39fa72dd155f8840c7
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2012-05-12 15:35:02 +0100

    Reordered

--- spec/fixed_odds_spec.rb
+++ spec/fixed_odds_spec.rb
@@ -229,16 +229,16 @@ describe "FixedOdds" do
     specify { FixedOdds.fractional_odds('3/2').profit_on_stake(Money.parse '£2').should == Money.parse('£3') }
   end
 
-  describe "#total_return_on_stake" do
-    specify { FixedOdds.fractional_odds('1/1').total_return_on_stake(Money.parse '£1').should == Money.parse('£2') }
-    specify { FixedOdds.fractional_odds('2/1').total_return_on_stake(Money.parse '£1').should == Money.parse('£3') }
-    specify { FixedOdds.fractional_odds('3/2').total_return_on_stake(Money.parse '£2').should == Money.parse('£5') }
-  end
-
   describe "#stake_to_profit" do
     specify { FixedOdds.fractional_odds('1/1').stake_to_profit(Money.parse '£1').should == Money.parse('£1') }
     specify { FixedOdds.fractional_odds('2/1').stake_to_profit(Money.parse '£2').should == Money.parse('£1') }
     specify { FixedOdds.fractional_odds('3/2').stake_to_profit(Money.parse '£3').should == Money.parse('£2') }
+  end
+
+  describe "#total_return_on_stake" do
+    specify { FixedOdds.fractional_odds('1/1').total_return_on_stake(Money.parse '£1').should == Money.parse('£2') }
+    specify { FixedOdds.fractional_odds('2/1').total_return_on_stake(Money.parse '£1').should == Money.parse('£3') }
+    specify { FixedOdds.fractional_odds('3/2').total_return_on_stake(Money.parse '£2').should == Money.parse('£5') }
   end
 
   describe "object comparison" do
