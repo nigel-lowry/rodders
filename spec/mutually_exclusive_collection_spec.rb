df4faa68f9dc440d7e6468da7ae47eada2857a0e
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-08-15 22:25:55 +0100

    Refactor

--- spec/mutually_exclusive_collection_spec.rb
+++ spec/mutually_exclusive_collection_spec.rb
@@ -24,27 +24,25 @@ describe "MutuallyExclusiveCollection" do
   end
 
   context "decimal odds arbitrage" do
-    before(:each) do
-      @bookmaker1outcome1 = FixedOdds.from_s '2.25'
-      @bookmaker1outcome2 = FixedOdds.from_s '4.9'
-      @bookmaker2outcome1 = FixedOdds.from_s '2.43'
-      @bookmaker2outcome2 = FixedOdds.from_s '3.85'
+    let(:bookmaker1outcome1) { FixedOdds.from_s '2.25' }
+    let(:bookmaker1outcome2) { FixedOdds.from_s '4.9' }
+    let(:bookmaker2outcome1) { FixedOdds.from_s '2.43' }
+    let(:bookmaker2outcome2) { FixedOdds.from_s '3.85' }
 
-      @bookmaker1 = MutuallyExclusiveCollection.new [@bookmaker1outcome1, @bookmaker1outcome2]
-      @bookmaker2 = MutuallyExclusiveCollection.new [@bookmaker2outcome1, @bookmaker2outcome2]
+    let(:bookmaker1) { MutuallyExclusiveCollection.new [bookmaker1outcome1, bookmaker1outcome2] }
+    let(:bookmaker2) { MutuallyExclusiveCollection.new [bookmaker2outcome1, bookmaker2outcome2] }
 
-      @bookmaker_vulnerable_to_arbitrage = MutuallyExclusiveCollection.new [@bookmaker2outcome1, @bookmaker1outcome2]
-    end
+    let(:bookmaker_vulnerable_to_arbitrage) { MutuallyExclusiveCollection.new [bookmaker2outcome1, bookmaker1outcome2] }
 
-    specify { @bookmaker1.should_not be_arbitrageable }
-    specify { @bookmaker2.should_not be_arbitrageable } 
-    specify { @bookmaker_vulnerable_to_arbitrage.should be_arbitrageable }
+    specify { bookmaker1.should_not be_arbitrageable }
+    specify { bookmaker2.should_not be_arbitrageable }
+    specify { bookmaker_vulnerable_to_arbitrage.should be_arbitrageable }
 
-    specify { @bookmaker1.bookmakers_return_rate.should be_within(0.0001).of(0.0534) }
-    specify { @bookmaker2.bookmakers_return_rate.should be_within(0.0001).of(0.0478) }
+    specify { bookmaker1.bookmakers_return_rate.should be_within(0.0001).of(0.0534) }
+    specify { bookmaker2.bookmakers_return_rate.should be_within(0.0001).of(0.0478) }
 
-    specify { @bookmaker_vulnerable_to_arbitrage.profit_on_stake('£100'.to_money).should == '£4.63'.to_money }
-    specify { @bookmaker_vulnerable_to_arbitrage.profit_percentage.should be_within(0.001).of(0.046) }
+    specify { bookmaker_vulnerable_to_arbitrage.profit_on_stake('£100'.to_money).should == '£4.63'.to_money }
+    specify { bookmaker_vulnerable_to_arbitrage.profit_percentage.should be_within(0.001).of(0.046) }
   end
 
   context "fractional odds arbitrage" do
@@ -115,7 +113,6 @@ describe "MutuallyExclusiveCollection" do
     let(:odds2) { FixedOdds.from_s '15/1' }
 
     let(:bookmaker_vulnerable_to_arbitrage) { MutuallyExclusiveCollection.new [odds1, odds2] }
-
 
     specify { bookmaker_vulnerable_to_arbitrage.should be_arbitrageable }
     specify { bookmaker_vulnerable_to_arbitrage.profit_on_stake('£100'.to_money).should == '£650'.to_money }
